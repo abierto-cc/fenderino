82c6c7855753f18ec56173ddae7c4b9b0670bb84
dcuartielles <dcuartielles@gmail.com>
2017-05-05 14:57:13 +0200

    Added Midi Example

---
+++ software/examples/FA_8_midi/pitches_midi.h
@@ -0,0 +1,108 @@
+/*************************************************
+ * MIDI Note numbers
+ *************************************************/
+
+ #define MIDI_NOTE_A0    21
+ #define MIDI_NOTE_AS0   22
+ #define MIDI_NOTE_B0    23
+
+ #define MIDI_NOTE_C1    24
+ #define MIDI_NOTE_CS1   25
+ #define MIDI_NOTE_D1    26
+ #define MIDI_NOTE_DS1   27
+ #define MIDI_NOTE_E1    28
+ #define MIDI_NOTE_F1    29
+ #define MIDI_NOTE_FS1   30
+ #define MIDI_NOTE_G1    31
+ #define MIDI_NOTE_GS1   32
+ #define MIDI_NOTE_A1    33
+ #define MIDI_NOTE_AS1   34
+ #define MIDI_NOTE_B1    35
+
+ #define MIDI_NOTE_C2    36
+ #define MIDI_NOTE_CS2   37
+ #define MIDI_NOTE_D2    38
+ #define MIDI_NOTE_DS2   39
+ #define MIDI_NOTE_E2    40
+ #define MIDI_NOTE_F2    41
+ #define MIDI_NOTE_FS2   42
+ #define MIDI_NOTE_G2    43
+ #define MIDI_NOTE_GS2   44
+ #define MIDI_NOTE_A2    45
+ #define MIDI_NOTE_AS2   46
+ #define MIDI_NOTE_B2    47
+
+ #define MIDI_NOTE_C3    48
+ #define MIDI_NOTE_CS3   49
+ #define MIDI_NOTE_D3    50
+ #define MIDI_NOTE_DS3   51
+ #define MIDI_NOTE_E3    52
+ #define MIDI_NOTE_F3    53
+ #define MIDI_NOTE_FS3   54
+ #define MIDI_NOTE_G3    55
+ #define MIDI_NOTE_GS3   56
+ #define MIDI_NOTE_A3    57
+ #define MIDI_NOTE_AS3   58
+ #define MIDI_NOTE_B3    59
+
+ #define MIDI_NOTE_C4    60
+ #define MIDI_NOTE_CS4   61
+ #define MIDI_NOTE_D4    62
+ #define MIDI_NOTE_DS4   63
+ #define MIDI_NOTE_E4    64
+ #define MIDI_NOTE_F4    65
+ #define MIDI_NOTE_FS4   66
+ #define MIDI_NOTE_G4    67
+ #define MIDI_NOTE_GS4   68
+ #define MIDI_NOTE_A4    69
+ #define MIDI_NOTE_AS4   70
+ #define MIDI_NOTE_B4    71
+
+ #define MIDI_NOTE_C5    72
+ #define MIDI_NOTE_CS5   73
+ #define MIDI_NOTE_D5    74
+ #define MIDI_NOTE_DS5   75
+ #define MIDI_NOTE_E5    76
+ #define MIDI_NOTE_F5    77
+ #define MIDI_NOTE_FS5   78
+ #define MIDI_NOTE_G5    79
+ #define MIDI_NOTE_GS5   80
+ #define MIDI_NOTE_A5    81
+ #define MIDI_NOTE_AS5   82
+ #define MIDI_NOTE_B5    83
+
+ #define MIDI_NOTE_C6    84
+ #define MIDI_NOTE_CS6   85
+ #define MIDI_NOTE_D6    86
+ #define MIDI_NOTE_DS6   87
+ #define MIDI_NOTE_E6    88
+ #define MIDI_NOTE_F6    89
+ #define MIDI_NOTE_FS6   90
+ #define MIDI_NOTE_G6    91
+ #define MIDI_NOTE_GS6   92
+ #define MIDI_NOTE_A6    93
+ #define MIDI_NOTE_AS6   94
+ #define MIDI_NOTE_B6    95
+
+ #define MIDI_NOTE_C7    96
+ #define MIDI_NOTE_CS7   97
+ #define MIDI_NOTE_D7    98
+ #define MIDI_NOTE_DS7   99
+ #define MIDI_NOTE_E7    100
+ #define MIDI_NOTE_F7    101
+ #define MIDI_NOTE_FS7   102
+ #define MIDI_NOTE_G7    103
+ #define MIDI_NOTE_GS7   104
+ #define MIDI_NOTE_A7    105
+ #define MIDI_NOTE_AS7   106
+ #define MIDI_NOTE_B7    107
+
+ #define MIDI_NOTE_C8    108
+ #define MIDI_NOTE_CS8   109
+ #define MIDI_NOTE_D8    110
+ #define MIDI_NOTE_DS8   111
+ #define MIDI_NOTE_E8    112
+ #define MIDI_NOTE_F8    113
+ #define MIDI_NOTE_FS8   114
+ #define MIDI_NOTE_G8    115
+ #define MIDI_NOTE_GS8   116
